f365fc9773e5d8fd40aa966cfaad0ce9c8c5e0f7
Juan Carlos Valladares Rea <jchobi_100@hotmail.com>
2025-11-16 03:11:26 -0600

    esta es la primer prueba

---
+++ Mexcametl/wwwroot/mapa-ess.js
@@ -0,0 +1,48 @@
+﻿window.mapaEss = (function () {
+
+    let dotNetRef = null;
+    let selectedPath = null;
+
+    function init(ref) {
+        dotNetRef = ref;
+
+        const container = document.querySelector(".map-container");
+        if (!container) return;
+
+        const svg = container.querySelector("svg");
+        if (!svg) return;
+
+        const states = svg.querySelectorAll('path[id^="MX"]');
+
+        states.forEach(path => {
+            // guarda el color original una sola vez
+            if (!path.dataset.originalFill) {
+                const attrFill = path.getAttribute("fill");
+                const cssFill = getComputedStyle(path).fill;
+                path.dataset.originalFill = attrFill || cssFill || "#5f8764";
+            }
+
+            path.addEventListener("click", () => {
+
+                // Quitar resaltado previo
+                if (selectedPath && selectedPath !== path) {
+                    const original = selectedPath.dataset.originalFill;
+                    selectedPath.style.fill = original;
+                }
+
+                // Aplicar color de selección al nuevo
+                path.style.fill = "#0e4a36"; // el verde oscuro de tu marca
+                selectedPath = path;
+
+                // Avisar a Blazor
+                if (dotNetRef) {
+                    dotNetRef.invokeMethodAsync("OnEstadoClick", path.id);
+                }
+            });
+        });
+    }
+
+    return {
+        init
+    };
+})();
